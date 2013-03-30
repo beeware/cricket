a7bdde227822ff2694b3b4f48a422980bf58fc1d
Russell Keith-Magee <russell@keith-magee.com>
2013-03-30 16:32:40 +0800

    Cleanup of setup.py

--- cricket/__init__.py
+++ cricket/__init__.py
@@ -1,4 +1,2 @@
 NUM_VERSION = (0, 1, 0)
 VERSION = ".".join(str(nv) for nv in NUM_VERSION)
-
-VERSION = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
-
-from distribute_setup import use_setuptools
-
-use_setuptools()
-
+#/usr/bin/env python
 from setuptools import setup
-from cricket import VERSION
+from xero import VERSION
 
 try:
-    readme = open("README.rst")
+    readme = open('README.rst')
     long_description = str(readme.read())
 finally:
     readme.close()
 
-setup(name="django-cricket",
+setup(
+    name='django-cricket',
     version=VERSION,
-    author="Russell Keith-Magee",
-    author_email="russell@keith-magee.com",
+    description='A graphical tool to assist running a Django test suite.',
+    long_description=long_description,
+    author='Russell Keith-Magee',
+    author_email='russell@keith-magee.com',
     url='http://pypi.python.org/pypi/django-cricket',
-    license="New BSD",
+    packages=['cricket'],
+    license='New BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -29,7 +28,5 @@ setup(name="django-cricket",
         'Topic :: Software Development :: Testing',
         'Topic :: Utilities',
     ],
-    platforms=["any"],
-    description="A graphical tool to assist running a Django test suite.",
-    long_description=long_description,
+    test_suite='tests'
 )
