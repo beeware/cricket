fb68619fe7549f0b9015f09b871a6fb1828de85e
Russell Keith-Magee <russell@keith-magee.com>
2013-04-01 09:57:11 +0800

    Factored events functionality into separate module.

--- cricket/model.py
+++ cricket/model.py
@@ -8,26 +8,7 @@ can bind to events on the model to be notified of changes.
 from datetime import datetime
 import subprocess
 
-
-class EventSource(object):
-    """A source of GUI events.
-
-    An event source can receive handlers for events, and
-    can emit events.
-    """
-    _events = {}
-
-    @classmethod
-    def bind(cls, event, handler):
-        cls._events.setdefault(cls, {}).setdefault(event, []).append(handler)
-
-    def emit(self, event, **data):
-        try:
-            for handler in self._events[self.__class__][event]:
-                handler(self, **data)
-        except KeyError:
-            # No handler registered for event.
-            pass
+from cricket.events import EventSource
 
 
 class TestMethod(EventSource):
