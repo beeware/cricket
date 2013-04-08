27635558d6f142bd9c9a46ebd82ff6e48ab7d2b2
Russell Keith-Magee <russell@keith-magee.com>
2013-04-08 11:02:19 +0800

    Corrected path to Django TestExecutor.

--- cricket/django/model.py
+++ cricket/django/model.py
@@ -5,8 +5,8 @@ class DjangoProject(Project):
 
     def discover_commandline(self):
         "Command lineDiscover all available tests in a project."
-        return ['python', 'manage.py', 'test', '--testrunner=cricket.runners.django.TestDiscoverer']
+        return ['python', 'manage.py', 'test', '--testrunner=cricket.django.runners.TestDiscoverer']
 
     def execute_commandline(self, labels):
         "Return the command line to execute the specified test labels"
-        return ['python', 'manage.py', 'test', '--testrunner=cricket.runners.django.TestExecutor', '--noinput'] + labels
+        return ['python', 'manage.py', 'test', '--testrunner=cricket.django.runners.TestExecutor', '--noinput'] + labels
