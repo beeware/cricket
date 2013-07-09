42c76bd506098554a56256c162c0a8b3157df647
Russell Keith-Magee <russell@keith-magee.com>
2013-07-09 09:44:06 +0800

    Refactored the main method into a common base class.

--- cricket/unittest/__main__.py
+++ cricket/unittest/__main__.py
@@ -1,36 +1,8 @@
-from Tkinter import *
-
-from cricket.view import MainWindow, TestLoadErrorDialog
-from cricket.model import ModelLoadError
+'''
+This is the main entry point for running unittest test suites.
+'''
+from cricket.main import main
 from cricket.unittest.model import PyTestProject
 
-
-def main():
-    # Set up the root Tk context
-    root = Tk()
-
-    # Construct an empty window
-    view = MainWindow(root)
-
-    # Try to load the project. If any error occurs during
-    # project load, show an error dialog
-    project = None
-    while project is None:
-        try:
-            project = PyTestProject()
-        except ModelLoadError, e:
-            TestLoadErrorDialog(root, e.trace)
-
-    # Set the project for the main window.
-    # This populates the tree, and sets listeners for
-    # future tree modifications.
-    view.project = project
-
-    # Run the main loop
-    try:
-        view.mainloop()
-    except KeyboardInterrupt:
-        view.on_quit()
-
 if __name__ == "__main__":
-    main()
+    main(PyTestProject)
