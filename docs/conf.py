3e938a58eb22ba8afab78cb54b4408f79cef6927
Russell Keith-Magee <russell@keith-magee.com>
2013-08-21 16:44:33 +0800

    Updated docs config file to remove references to django-cricket.

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Django-Cricket documentation build configuration file, created by
+# Cricket documentation build configuration file, created by
 # sphinx-quickstart on Sat Feb  9 10:44:39 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Django-Cricket'
+project = u'Cricket'
 copyright = u'2013, Russell Keith-Magee'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Django-Cricketdoc'
+htmlhelp_basename = 'Cricketdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Django-Cricket.tex', u'Django-Cricket Documentation',
+  ('index', 'Cricket.tex', u'Cricket Documentation',
    u'Russell Keith-Magee', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'django-cricket', u'Django-Cricket Documentation',
+    ('index', 'cricket', u'Cricket Documentation',
      [u'Russell Keith-Magee'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Django-Cricket', u'Django-Cricket Documentation',
-   u'Russell Keith-Magee', 'Django-Cricket', 'One line description of project.',
+  ('index', 'Cricket', u'Cricket Documentation',
+   u'Russell Keith-Magee', 'Cricket', 'A graphical tool to assist running test suites.',
    'Miscellaneous'),
 ]
 
