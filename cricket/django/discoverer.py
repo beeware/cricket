d842d8011f79aae360e82f8d55494c471f147118
Kirill Borisov <lensvol@gmail.com>
2013-09-15 01:31:12 +0400

    Runner now supports embedded test suites in build_suite() result.

--- cricket/django/discoverer.py
+++ cricket/django/discoverer.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+from unittest import TestSuite
+
 from django.conf import settings
 from django.test.simple import DjangoTestSuiteRunner
 from django.test.utils import get_runner
@@ -14,16 +16,22 @@ class TestDiscoverer(TestRunnerClass):
     Doesn't actually run any of the tests.
     """
     def run_tests(self, test_labels, extra_tests=None, **kwargs):
-        for test in self.build_suite(test_labels):
-            # Django 1.6 introduce the new-style test runner.
-            # If that test runner is in use, we use the full test name.
-            # If we're still using a pre 1.6-style runner, we need to
-            # drop out all everything between the app name and the test module.
-            if issubclass(TestRunnerClass, DjangoTestSuiteRunner):
-                parts = test.id().split('.')
-                tests_index = parts.index('tests')
-                print '%s.%s.%s' % (parts[tests_index - 1], parts[-2], parts[-1])
-            else:
-                print test.id()
+        def walk_suite(suite):
+            for test in suite:
+                if isinstance(test, TestSuite):
+                    walk_suite(test)
+                    continue
+                # Django 1.6 introduce the new-style test runner.
+                # If that test runner is in use, we use the full test name.
+                # If we're still using a pre 1.6-style runner, we need to
+                # drop out all everything between the app name and the test module.
+                if issubclass(TestRunnerClass, DjangoTestSuiteRunner):
+                    parts = test.id().split('.')
+                    tests_index = parts.index('tests')
+                    print '%s.%s.%s' % (parts[tests_index - 1], parts[-2], parts[-1])
+                else:
+                   print test.id()
+
+        walk_suite(self.build_suite(test_labels))
 
         return 0
