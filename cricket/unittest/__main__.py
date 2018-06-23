26913511fe00008ed78ffd4f2695f26924c11e45
Russell Keith-Magee <russell@keith-magee.com>
2018-06-23 12:03:58 +0800

    Updated Unittest runner to use new pytest-compatible API.

--- cricket/unittest/__main__.py
+++ cricket/unittest/__main__.py
@@ -9,5 +9,9 @@ def main():
     return cricket_main(UnittestTestSuite)
 
 
-if __name__ == "__main__":
+def run():
     main().main_loop()
+
+
+if __name__ == "__main__":
+    run()
--- cricket/unittest/model.py
+++ cricket/unittest/model.py
@@ -1,10 +1,9 @@
 import sys
 
-from cricket.model import TestSuite
+from cricket.model import TestSuite, TestModule, TestCase, TestMethod
 
 
 class UnittestTestSuite(TestSuite):
-
     def __init__(self, options=None):
         super(UnittestTestSuite, self).__init__()
 
@@ -18,3 +17,20 @@ class UnittestTestSuite(TestSuite):
         if self.coverage:
             args.append('--coverage')
         return args + labels
+
+    def split_test_id(self, test_id):
+        pathparts = test_id.split('.')
+
+        return [
+            (TestModule, part)
+            for part in pathparts[:-2]
+        ] + [
+            (TestCase, pathparts[-2]),
+            (TestMethod, pathparts[-1]),
+        ]
+
+    def join_path(self, parent, klass, part):
+        if parent.path is None:
+            return part
+        else:
+            return '{}.{}'.format(parent.path, part)
